0934f8a43d168ccf0a07530f818b0611d39780bc
Тимур Чащин <minikustik2006@gmail.com>
2025-10-18 14:52:32 +0500

    выполнение задания

---
+++ js/functions.js
@@ -0,0 +1,21 @@
+// Функция проверки длины строки
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
+
+// Функция проверки палиндрома с учётом пробелов и регистра
+const isPalindrome = (string) => {
+  const normalizedString = string.replaceAll(' ', '').toLowerCase();
+  const reversedString = normalizedString.split('').reverse().join('');
+  return normalizedString === reversedString;
+};
+
+// Проверка длины строки
+console.log(checkStringLength('проверяемая строка', 20));
+console.log(checkStringLength('проверяемая строка', 18));
+console.log(checkStringLength('проверяемая строка', 10));
+
+// Проверка палиндрома
+console.log(isPalindrome('топот'));
+console.log(isPalindrome('ДовОд'));
+console.log(isPalindrome('Кекс'));
+console.log(isPalindrome('Лёша на полке клопа нашёл '));
+
